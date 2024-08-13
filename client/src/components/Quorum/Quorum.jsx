a2339503d3e317f97b56a249963b7b19e86aff23
Carlos Castellanos <karlos065@hotmail.com>
2024-08-13 16:51:04 -0500

    Update Quorum.jsx

--- client/src/components/Quorum/Quorum.jsx
+++ client/src/components/Quorum/Quorum.jsx
@@ -62,7 +62,7 @@ const Quorum = () => {
         <Pie percent={porcentajeUsuarios} porcentajeData={porcentajeUsuarios} />
       </div>
       <div className="p-5 space-y-5 overflow-y-auto font-extrabold text-white bg-black rounded-lg opacity-70">
-        <div className="bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+        <div className="bg-white text-black rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
           <div className="border-2 border-black rounded-lg space-y-2 lg:space-y-6 p-2 lg:p-8">
             <div
               className={`bg-white rounded-lg p-1 ${
